d1383a08d0d11864c8a94c2b2ce26d2b415f3193
Shanmugam Chinnappa <shan@ext.grexel.com>
2020-10-01 09:22:05 +0300

    Enabled swagger (default page is not swagger yet)

--- DotNet5Exploratory/Startup.cs
+++ DotNet5Exploratory/Startup.cs
@@ -26,6 +26,9 @@ namespace DotNet5Exploratory
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +40,17 @@ namespace DotNet5Exploratory
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
